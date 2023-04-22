3ff0febe7f9c8cc72bd464ebfe5cd44eed03ad1b
Jarad Niemi <niemi@iastate.edu>
2023-04-22 06:28:47 -0500

    Add easter 2023 message

---
+++ easter2023.R
@@ -0,0 +1,6 @@
+my_cipher <- create_cipher(seed = 20230409)
+
+my_message <- "Search high and low, but probably low, to find the treasure that you seek."
+
+my_encrypted_message <- encrypt(my_message, my_cipher)
+
